f83133d23698a0a21a237ec356c17fd904152c24
NOJaeHyuk <jaehyuk7722@naver.com>
2024-11-18 10:19:49 +0900

    first commit

---
+++ src/main/java/com/spartaweb/SpartaWebApplication.java
@@ -0,0 +1,13 @@
+package com.spartaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spartaweb/SpartaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spartaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
